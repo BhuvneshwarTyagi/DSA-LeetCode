c2a4e7867bd3465a1c9c91f9053e825604165606
Bhuvneshwar Tyagi <83375364+BhuvneshwarTyagi@users.noreply.github.com>
2023-06-03 18:12:29 +0530

    Time: 15 ms (87.43%), Space: 20.3 MB (72.7%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -4,17 +4,21 @@ public:
         int len=flowerbed.size();
         int count=0;
         if(len>1){for(int i=0;i<len;i++){
-            if(i==0 && flowerbed[i+1]==0 && flowerbed[i]==0){
-                flowerbed[i]=1;
-                count++;
-            }
-            if(i==len-1 && flowerbed[i-1]==0 && flowerbed[i]==0){
-                flowerbed[i]=1;
-                count++;
-            }
             if(i>0 && i<len-1 && flowerbed[i-1]==0 && flowerbed[i+1]==0 && flowerbed[i]==0){
                 flowerbed[i]=1;
                 count++;
+            }
+            else{
+                if(i==0 && flowerbed[i+1]==0 && flowerbed[i]==0){
+                flowerbed[i]=1;
+                count++;
+            }
+            else{
+                if(i==len-1 && flowerbed[i-1]==0 && flowerbed[i]==0){
+                flowerbed[i]=1;
+                count++;
+            }
+            }
             }
         }}
         else{
