5b5c9dc2b7e9fcd606120504433ad952d510a49a
Bhuvneshwar Tyagi <83375364+BhuvneshwarTyagi@users.noreply.github.com>
2023-06-04 14:32:04 +0530

    Time: 3 ms (50.16%), Space: 8.6 MB (80.36%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -3,25 +3,21 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         int n=digits.size();
         int carry=1;
-        vector<int> output;
-
-        for(int i=0;i<n;i++){
-            output.push_back(digits[i]);
-        }
+        
         for(int i=n-1;i>=0;i--){
-            output[i]+=carry;
-            carry=output[i]/10;
+            digits[i]+=carry;
+            carry=digits[i]/10;
             
             if(!carry){
                 break;
             }else{
-                output[i]=output[i]%10;
+                digits[i]=digits[i]%10;
             }
         }
         if(carry!=0){
-            output.insert(output.begin(),carry);
+            digits.insert(digits.begin(),carry);
         }
 
-        return output;
+        return digits;
     }
 };
