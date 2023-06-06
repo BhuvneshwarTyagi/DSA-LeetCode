70258f31ee6a504ce801bb5c57d5cfcd16b7deab
Bhuvneshwar Tyagi <83375364+BhuvneshwarTyagi@users.noreply.github.com>
2023-06-06 21:25:48 +0530

    Time: 13 ms (16.87%), Space: 15.1 MB (5.8%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -0,0 +1,101 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+
+        ListNode* head = nullptr;
+        ListNode* tail = nullptr;
+
+        ListNode* temp1 = list1;
+        ListNode* temp2 = list2;
+        while(temp1!=nullptr && temp2!=nullptr){
+                ListNode *ne=new ListNode();
+                if(head==nullptr){
+                    head=ne;
+                }
+                if(temp1->val < temp2->val){
+                    ne->val=temp1->val;
+                    if(tail==nullptr){
+                        tail=ne;
+                    }
+                    else{
+                        tail->next=ne;
+                        tail=ne;
+                    }
+                    temp1=temp1->next;
+                }
+                else{
+                    if(temp1->val > temp2->val){
+                        ne->val=temp2->val;
+                        if(tail==nullptr){
+                            tail=ne;
+                        }
+                        else{
+                            tail->next=ne;
+                            tail=ne;
+                        }
+                        temp2=temp2->next;
+                    }
+                    else{
+                        ne->val=temp2->val;
+                        if(tail==nullptr){
+                            tail=ne;
+                        }
+                        else{
+                            tail->next=ne;
+                            tail=ne;
+                        }
+                        temp2=temp2->next;
+                        ListNode *ne=new ListNode();
+                        ne->val=temp1->val;
+                        if(tail==nullptr){
+                            tail=ne;
+                        }
+                        else{
+                            tail->next=ne;
+                            tail=ne;
+                        }
+                        temp1=temp1->next;
+                    }
+                }
+            }
+            while(temp1!=nullptr){
+                ListNode *ne=new ListNode();
+                ne->val=temp1->val;
+                temp1=temp1->next;
+                if(tail==nullptr){
+                        tail=ne;
+                        head=ne;
+                    }
+                    else{
+                        tail->next=ne;
+                        tail=ne;
+                    }
+                
+            }
+            while(temp2!=nullptr){
+                ListNode *ne=new ListNode();
+                ne->val=temp2->val;
+                temp2=temp2->next;
+                if(tail==nullptr){
+                    head=ne;
+                        tail=ne;
+                    }
+                    else{
+                        tail->next=ne;
+                        tail=ne;
+                    }
+                
+            }
+            return head;
+    }
+};
